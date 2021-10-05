f01b80508676541b54f980c87ae52bb0c85aecb8
HariyonoRizki2 <69133831+HariyonoRizki2@users.noreply.github.com>
2021-10-06 06:46:07 +0700

    Delete start2.sh

--- start2.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
-cd /VCPlayerBot
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 main.py
